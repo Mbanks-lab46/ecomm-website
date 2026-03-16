0b2a07dfcb736e84a0517f6ca1d5c02ff81b6836
Mbanks-lab46 <85198700+Mbanks-lab46@users.noreply.github.com>
2026-03-15 20:51:06 -0500

    Fourth Commit

--- src/app/components/product-list/product-list.ts
+++ src/app/components/product-list/product-list.ts
@@ -1,20 +1,50 @@
-import { Component, inject } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, signal, OnInit } from '@angular/core';
+import { CommonModule, AsyncPipe } from '@angular/common';
 import { Product } from '../../services/product';
 import { Products } from '../../types/product.interface';
-import { Observable } from 'rxjs';
+import { RouterLink, ActivatedRoute } from '@angular/router';
+import { Observable, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AsyncPipe, RouterLink],
   templateUrl: './product-list.html',
   styleUrl: './product-list.scss',
 })
-export class ProductList {
+export class ProductList implements OnInit {
   private productService = inject(Product);
-  products: Products[] = [];
-  productList$: Observable<Products[]> = this.productService.getAll();
+  private route = inject(ActivatedRoute);
+  products = signal<Products[]>([]);
+  filteredProducts = signal<Products[]>([]);
+  searchQuery = signal<string>('');
+  
+  ngOnInit(): void {
+    this.productService.getAll().subscribe(data => {
+      this.products.set(data);
+      this.filteredProducts.set(data);
+    });
+
+    this.route.queryParams.subscribe(params => {
+      const query = params['search'] || '';
+      this.searchQuery.set(query);
+      this.filterProducts(query);
+    })
+  }
+
+  filterProducts(query: string) {
+    if (!query.trim()) {
+      this.filteredProducts.set(this.products());
+      return;
+    }
+    const lower = query.toLowerCase();
+    this.filteredProducts.set(
+      this.products().filter(p =>
+        p.name.toLowerCase().includes(lower) ||
+        p.description.toLowerCase().includes(lower)
+      )
+    );
+  }
 
  
 }
